5f497ea2624d0140dc83097b1fd3a69a9f78b87f
Joel Wejdenstål <jwejdenstal@icloud.com>
2025-01-22 16:21:53 +0100

    add class decl

--- lab1/time.hpp
+++ lab1/time.hpp
@@ -1,10 +1,53 @@
 #ifndef TIME_HPP
 #define TIME_HPP
 
-class Timestamp
+#include <string>
+#include <iostream>
+
+enum class TimeFormat
+{
+    TWENTYY_FOUR_HOUR,
+    TWELVE_HOUR,
+};
+
+class Time
 {
 public:
-    Timestamp();
+    Time(int hour, int minute, int second); // TODO: initialization list
+    Time(std::string time);
+
+    std::string to_string(TimeFormat format) const;
+
+    bool is_am() const;
+
+    int get_hour() const;
+    int get_minute() const;
+    int get_second() const;
+
+    Time& operator++();
+    Time operator++(int) const;
+
+    Time& operator--();
+    Time operator--(int) const;
+
+    bool operator==(const Time& rhs) const;
+    bool operator!=(const Time& rhs) const;
+
+    bool operator<(const Time& rhs) const;
+    bool operator>(const Time& rhs) const;
+    bool operator<=(const Time& rhs) const;
+    bool operator>=(const Time& rhs) const;
+
+    Time operator+(const Time& rhs) const;
+    Time& operator+=(const Time& rhs);
+    Time operator-(const Time& rhs) const;
+    Time& operator-=(const Time& rhs);
+
+private:
+    int secs;
 };
+
+std::ostream& operator<<(std::ostream& stream, const Time& time);
+std::ostream& operator>>(std::ostream& stream, const Time& time);
 
 #endif
