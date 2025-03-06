1aab937e5eb9a75222bfb782acbdbb3e15731b08
matol699 <matol699@su11-203.ad.liu.se>
2025-03-06 15:22:52 +0100

    remove local to_string function

--- lab2/test.cpp
+++ lab2/test.cpp
@@ -1,17 +1,6 @@
 #include "../lib/catch.hpp"
 #include "list.h"
 
-#include <string>
-#include <iostream>
-#include <sstream>
-
-std::string to_string(const List &l)
-{
-    std::stringstream ss;
-    ss << l;
-    return ss.str();
-}
-
 TEST_CASE("list")
 {
     List l1;
@@ -38,43 +27,43 @@ TEST_CASE("list")
     CHECK(sub.at(1) == 6);
     CHECK(sub.at(2) == 8);
 
-    CHECK(to_string(l1) == "[3, 4, 5, 6, 7, 8, 9]");
-    CHECK(to_string(sub) == "[4, 6, 8]");
+    CHECK(l1.to_string() == "[3, 4, 5, 6, 7, 8, 9]");
+    CHECK(sub.to_string() == "[4, 6, 8]");
 
     l1.insert(13);
     CHECK(l1.back() == 13);
 
     l1.insert(6);
     l1.insert(8);
-    CHECK(to_string(l1) == "[3, 4, 5, 6, 6, 7, 8, 8, 9, 13]");
+    CHECK(l1.to_string() == "[3, 4, 5, 6, 6, 7, 8, 8, 9, 13]");
 
     List l2;
     l2.insert(3);
     l2.insert(75);
     l2.insert(13);
     l2.insert(1);
-    CHECK(to_string(l2) == "[1, 3, 13, 75]");
+    CHECK(l2.to_string() == "[1, 3, 13, 75]");
 
     l2.remove(2);
-    CHECK(to_string(l2) == "[1, 3, 75]");
+    CHECK(l2.to_string() == "[1, 3, 75]");
 
     l1.remove(1);
-    CHECK(to_string(l1) == "[3, 5, 6, 6, 7, 8, 8, 9, 13]");
+    CHECK(l1.to_string() == "[3, 5, 6, 6, 7, 8, 8, 9, 13]");
 
     l1.remove(0);
-    CHECK(to_string(l1) == "[5, 6, 6, 7, 8, 8, 9, 13]");
+    CHECK(l1.to_string() == "[5, 6, 6, 7, 8, 8, 9, 13]");
 
     l1.remove(7);
-    CHECK(to_string(l1) == "[5, 6, 6, 7, 8, 8, 9]");
+    CHECK(l1.to_string() == "[5, 6, 6, 7, 8, 8, 9]");
 
     SECTION("Copy constructor")
     {
         List l1{4, 5};
         List l2{l1};
 
-        CHECK(to_string(l1) == to_string(l2));
+        CHECK(l1.to_string() == l2.to_string());
         l2.insert(7);
-        CHECK_FALSE(to_string(l1) == to_string(l2));
+        CHECK_FALSE(l1.to_string() == l2.to_string());
     }
 
     SECTION("Move constructor")
@@ -82,7 +71,7 @@ TEST_CASE("list")
         List l1{4, 5};
         List l2{std::move(l1)};
 
-        CHECK(to_string(l2) == "[4, 5]");
+        CHECK(l2.to_string() == "[4, 5]");
         CHECK(l1.is_empty());
     }
 
@@ -92,9 +81,9 @@ TEST_CASE("list")
         List l2{};
         l2 = l1;
 
-        CHECK(to_string(l1) == to_string(l2));
+        CHECK(l1.to_string() == l2.to_string());
         l2.insert(7);
-        CHECK_FALSE(to_string(l1) == to_string(l2));
+        CHECK_FALSE(l1.to_string() == l2.to_string());
     }
 
     SECTION("Move assignment")
@@ -103,7 +92,7 @@ TEST_CASE("list")
         List l2{};
         l2 = std::move(l1);
 
-        CHECK(to_string(l2) == "[4, 5]");
+        CHECK(l2.to_string() == "[4, 5]");
         CHECK(l1.is_empty());
     }
 }
